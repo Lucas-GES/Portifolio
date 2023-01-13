ba888a5085c9ba5329b20eeca95e91f071d6af2d
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2023-01-13 18:03:12 -0300

    Finished Home and About page

--- src/Component/List.js
+++ src/Component/List.js
@@ -1,17 +1,18 @@
 import React from "react"
-import { NavLink, BrowserRouter } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../Css/ListStyle.css';
 
 const List = () => {
+
     return (
         <>
                 <header>
                     <div className="container">
                         <nav>
                             <div className="list">
-                                <NavLink to='/' className='listItem'>Home</NavLink>
-                                <NavLink to='/contato' className='listItem'>Contato</NavLink>
+                                <NavLink to='/' className='listItem home'>Home</NavLink>
                                 <NavLink to='/sobre' className='listItem'>Sobre</NavLink>
+                                <NavLink to='/contato' className='listItem'>Contato</NavLink>
                                 <NavLink to='/projetos' className='listItem'>Projetos</NavLink>
                             </div>
                         </nav>
--- src/Component/Sobre.js
+++ src/Component/Sobre.js
@@ -3,9 +3,29 @@ import '../Css/SobreStyle.css';
 
 const Sobre = () => {
     return (
-        <div className="container">
-            <h1>Sobre</h1>
-        </div>
+        <>
+            <div className="sobreContainer">
+                <div className="sobreImage">
+                    <img src={require('../Images/eu.jpeg')} alt="me" />
+                </div>
+                <div className="sobreText">
+                    <h3>Olá meu nome é Lucas Gabriel do Espirito Santo</h3>
+                    <h3>Sou formado em Engenharia de Software</h3>
+                    <h3>Eterno estudante, porque nessa área e na nossa vida, <br /> nunca paramos de aprender</h3>
+                    <h3>Abaixo estão algumas de minhas skills:</h3>
+                    <img style={{width: '30%'}} src={require('../Images/arrowdown.png')} alt="" />
+                </div>
+            </div>
+            <div>
+                <img style={{ width: '20%', margin: '5%' }} src={require('../Images/web.png')} alt="" />
+                <img style={{ width: '8%', margin: '5%' }} src={require('../Images/python.png')} alt="" />
+                <img style={{ width: '15%', margin: '5%' }} src={require('../Images/java.png')} alt="" />
+                <img style={{ width: '15%', margin: '5%' }} src={require('../Images/react.png')} alt="" />
+                <img style={{ width: '20%', margin: '5%' }} src={require('../Images/laravel.png')} alt="" />
+                <img style={{ width: '10%', margin: '5%' }} src={require('../Images/vuejs.png')} alt="" />
+                <img style={{ width: '10%', margin: '5%' }} src={require('../Images/mysql.png')} alt="" />
+            </div>
+        </>
     )
 
 }
